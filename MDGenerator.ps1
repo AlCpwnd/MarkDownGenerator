620e8ed97d867cd16d5973f2a3aa08f4cb819787
AlCapwnd <a.dejaeger@hotmail.com>
2023-10-14 00:22:34 +0200

    Changed from script to function for usability. Improved documentation.

--- MDGenerator.ps1
+++ MDGenerator.ps1
@@ -1,127 +1,144 @@
-[cmdletbinding()]
-param(
-    [ValidateScript({
-        if( -Not ($_ | Test-Path -Filter *.ps1) ){
-            throw 'File does not exist'
+function Write-MDDocumentation{
+    [cmdletbinding()]
+    param(
+        [ValidateScript({
+            if( -Not ($_ | Test-Path -Filter *.ps1) ){
+                throw 'File does not exist'
+            }
+            return $true
+        })]
+        [Parameter(
+            Mandatory=$true, 
+            ValueFromPipelineByPropertyName=$true,
+            HelpMessage='Path to script for which you want to generate documentation.'
+        )]
+        [Alias('FullName')]
+        [String]$Script,    
+        [ValidateScript({
+            if( -Not ($_ | Test-Path -Filter *.md) ){
+                throw 'File does not exist'
+            }
+            return $true
+        })]
+        [String]$Append
+    )
+    
+    process{
+        Write-Verbose "Recovering help description for the script."
+        $HelpContent = Get-Help $Script -Full
+        $ScriptName = [String]$Script.Split('\')[-1]
+        $FileName = $HelpContent.Name.Replace('.ps1','.md')
+        Write-Verbose "Creating file: $FileName"
+
+        "# $ScriptName" | Out-File -FilePath $FileName
+
+        if($HelpContent.Synopsis){
+            Write-Verbose "Adding Synopsis..."
+            "",'## Synopsis',$HelpContent.Synopsis | ForEach-Object{Add-Content -Path $FileName -Value $_}
         }
-        return $true
-    })]
-    [String]$Script,    
-    [ValidateScript({
-        if( -Not ($_ | Test-Path -Filter *.md) ){
-            throw 'File does not exist'
+
+        Write-Verbose "Adding Syntax..."
+        # $Syntax = ($HelpContent.syntax | Out-String).Trim().Split('\') | Where-Object{$_ -match $FileName}
+        $Syntax = (($HelpContent.syntax | Out-String) -Replace '[A-Z]?:?\\.+\\',';').Split(';').Trim()
+        "",'## Syntax'| Add-Content -Path $FileName
+        foreach($CodeBlock in $Syntax){
+            if($CodeBlock -eq ''){
+                Continue
+            }
+            '```',"$CodeBlock",'```'| Add-Content -Path $FileName
         }
-        return $true
-    })]
-    [String]$Append
-)
 
-Write-Verbose "Recovering help description for the script."
-$HelpContent = Get-Help $Script -Full
-$ScriptName = [String]$Script.Split('\')[-1]
-$FileName = $HelpContent.Name.Replace('.ps1','.md')
-Write-Verbose "Creating file: $FileName"
+        if($HelpContent.description.Text){
+            Write-Verbose "Adding Description..."
+            "",'## Description',$HelpContent.description.Text.Trim() | Add-Content -Path $FileName
+        }
 
-"# $ScriptName" | Out-File -FilePath $FileName
+        if($HelpContent.examples){
+            Write-Verbose "Adding Examples..."
+            "",'## Examples' | Add-Content -Path $FileName
+            $Examples = $HelpContent.examples.example
+            $Count = 1
+            foreach($Example in $Examples){
+                "","### Example $Count",'```ps',$Example.code,'```' | Add-Content -Path $FileName
+            }
+        }
 
-if($HelpContent.Synopsis){
-    Write-Verbose "Adding Synopsis..."
-    "",'## Synopsis',$HelpContent.Synopsis | ForEach-Object{Add-Content -Path $FileName -Value $_}
-}
+        if($HelpContent.parameters){
+            Write-Verbose "Adding Parameters..."
+            "",'## Parameters' | Add-Content -Path $FileName
+            $Parameters = $HelpContent.parameters.parameter
+            foreach($Parameter in $Parameters){
+                "","### -$($Parameter.Name)",$Parameter.Description.Text,'```',"Type: $($Parameter.type.Name)",'Parameter Sets: (All)',"","Required: $($Parameter.required)","Position: $($Parameter.Position)","Default value: $(if($Parameter.defaultValue){$Parameter.defaultValue}else{"None"})","Accept pipeline: $($Parameter.pipelineInput)","Accept wildcard characters: $($Parameter.globbing)",'```' | Add-Content -Path $FileName
+            }
+        }
 
-Write-Verbose "Adding Syntax..."
-# $Syntax = ($HelpContent.syntax | Out-String).Trim().Split('\') | Where-Object{$_ -match $FileName}
-$Syntax = (($HelpContent.syntax | Out-String) -Replace '[A-Z]?:?\\.+\\',';').Split(';').Trim()
-"",'## Syntax'| Add-Content -Path $FileName
-foreach($CodeBlock in $Syntax){
-    if($CodeBlock -eq ''){
-        Continue
-    }
-    '```',"$CodeBlock",'```'| Add-Content -Path $FileName
-}
+        if($HelpContent.inputType){
+            Write-Verbose "Adding InputType..."
+            "",'## Inputs',$HelpContent.inputTypes.inputType.type.Name | Add-Content -Path $FileName
+        }
 
-if($HelpContent.description.Text){
-    Write-Verbose "Adding Description..."
-    "",'## Description',$HelpContent.description.Text.Trim() | Add-Content -Path $FileName
-}
+        if($HelpContent.returnValue){
+            Write-Verbose "Adding Returnvalues..."
+            "",'## Outputs',$HelpContent.returnValues.returnValue.type.name | Add-Content -Path $FileName
+        }
 
-if($HelpContent.examples){
-    Write-Verbose "Adding Examples..."
-    "",'## Examples' | Add-Content -Path $FileName
-    $Examples = $HelpContent.examples.example
-    $Count = 1
-    foreach($Example in $Examples){
-        "","### Example $Count",'```ps',$Example.code,'```' | Add-Content -Path $FileName
-    }
-}
+        if($HelpContent.relatedLinks){
+            Write-Verbose "Adding Links..."
+            "",'## Related Links' | Add-Content -Path $FileName
+            $Links = $HelpContent.relatedLinks.navigationLink.linkText
+            foreach($Link in $Links){
+                try{
+                    $Url = (Get-Help $Link -ErrorAction Stop).relatedLinks.navigationLink.uri | Where-Object{$_ -ne $null}
+                    "* [$Link]($Url)" | Add-Content -Path $FileName
+                }catch{
+                    continue
+                }
+            }
+        }
 
-if($HelpContent.parameters){
-    Write-Verbose "Adding Parameters..."
-    "",'## Parameters' | Add-Content -Path $FileName
-    $Parameters = $HelpContent.parameters.parameter
-    foreach($Parameter in $Parameters){
-        "","### -$($Parameter.Name)",$Parameter.Description.Text,'```',"Type: $($Parameter.type.Name)",'Parameter Sets: (All)',"","Required: $($Parameter.required)","Position: $($Parameter.Position)","Default value: $(if($Parameter.defaultValue){$Parameter.defaultValue}else{"None"})","Accept pipeline: $($Parameter.pipelineInput)","Accept wildcard characters: $($Parameter.globbing)",'```' | Add-Content -Path $FileName
-    }
-}
-
-if($HelpContent.inputType){
-    Write-Verbose "Adding InputType..."
-    "",'## Inputs',$HelpContent.inputTypes.inputType.type.Name | Add-Content -Path $FileName
-}
-
-if($HelpContent.returnValue){
-    Write-Verbose "Adding Returnvalues..."
-    "",'## Outputs',$HelpContent.returnValues.returnValue.type.name | Add-Content -Path $FileName
-}
-
-if($HelpContent.relatedLinks){
-    Write-Verbose "Adding Links..."
-    "",'## Related Links' | Add-Content -Path $FileName
-    $Links = $HelpContent.relatedLinks.navigationLink.linkText
-    foreach($Link in $Links){
-        try{
-            $Url = (Get-Help $Link -ErrorAction Stop).relatedLinks.navigationLink.uri | Where-Object{$_ -ne $null}
-            "* [$Link]($Url)" | Add-Content -Path $FileName
-        }catch{
-            continue
+        if(-not $Append){
+            Write-Verbose "Done generating file: $FileName"
+        }else{
+            $MD = Get-Content -Path $FileName
+            '','---','' | Add-Content -Path $Append
+            $MD | ForEach-Object{$_.Replace('# ','## ') | Add-Content -Path $Append}
+            Write-Verbose "Content added to: $Append"
         }
     }
+
+    <#
+        .SYNOPSIS
+        Will generate a documentation file based on the given script.
+
+        .DESCRIPTION
+        Will generate a markdown file based on the content of your scripts comments in the script's current directory.
+
+        .PARAMETER Script
+        Path to the script for which you want to generate the file.
+
+        .PARAMETER Append
+        Existing MD file you want to append the generated report to.
+
+        .INPUTS
+        Multiple scripts can be piped into this function
+
+        .OUTPUTS
+        Will generate a MD containing documentation for the give script.
+        Or wil append the documentation to an existing MD file.
+
+        .EXAMPLE
+        PS> Write-MDDocumentation -Script .\Test.ps1
+
+        .EXAMPLE
+        PS> Get-Childitem -Filter *.ps1 | Write-MDDocumentation
+
+        .EXAMPLE
+        PS> Get-Childitem -Filter *.ps1 | Write-MDDocumentation -Append .\README.md -Verbose
+
+        .LINK
+        Get-Help
+
+        .LINK
+        Add-Content
+    #>
 }
-
-if(-not $Append){
-    Write-Verbose "Done generating file: $FileName"
-}else{
-    $MD = Get-Content -Path $FileName
-    "" | Add-Content -Path $Append
-    $MD | ForEach-Object{$_.Replace('# ','## ') | Add-Content -Path $Append}
-    Write-Verbose "Content added to: $Append"
-}
-
-<#
-    .SYNOPSIS
-    Will generate a documentation file based on the given script.
-
-    .DESCRIPTION
-    Will generate a markdown file based on the content of your scripts comments in the script's current directory.
-
-    .PARAMETER Script
-    Path to the script for which you want to generate the file.
-
-    .PARAMETER Append
-    Existing MD file you want to append the generated report to.
-
-    .INPUTS
-    None.
-
-    .OUTPUTS
-    None.
-
-    .EXAMPLE
-    MDGenerator.ps1 -Script .\Test.ps1
-
-    .LINK
-    Get-Help
-
-    .LINK
-    Add-Content
-#>
